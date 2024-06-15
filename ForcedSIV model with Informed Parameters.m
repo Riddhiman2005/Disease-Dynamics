19922b2f02a8552c2771ea71c266255f84d57720
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2024-06-15 07:31:40 +0530

    Create ForcedSIV model with Informed Parameters.m

---
+++ ForcedSIV model with Informed Parameters.m
@@ -0,0 +1,18 @@
+clear all;
+close all;
+clc;
+options = odeset('RelTol', 1e-4, 'NonNegative', [1 2 3]);
+figure
+[t, x] = ode45('siv', [0 171], [8000 1000 1000], options);
+plot(t, x(:,1), 'b.', t, x(:,2), 'r.', t, x(:,3), 'g.')
+legend('S_1', 'I_1', 'V_1')
+hold on
+[t, x] = ode45('siv', [0 171], [1000 2000 7000], options);
+plot(t, x(:,1), 'b:', t, x(:,2), 'r:', t, x(:,3), 'g:')
+hold on
+[t, x] = ode45('siv', [0 171], [5500 500 4000], options);
+plot(t, x(:,1), 'b-', t, x(:,2), 'r-', t, x(:,3), 'g-')
+xlabel('Months')
+ylabel('Individuals Infected')
+legend('S_1', 'I_1', 'V_1', 'S_2', 'I_2', 'V_2', 'S_3', 'I_3', 'V_3', 'Location','best')
+title('Forced SIV Model with Informed Parameters')
