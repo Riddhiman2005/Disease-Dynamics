41a3128ae88781b50af7fbbcbe4327dfc24bcede
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2024-06-15 07:52:25 +0530

    Update ForcedSIV model with Informed Parameters.m

--- ForcedSIV model with Informed Parameters.m
+++ ForcedSIV model with Informed Parameters.m
@@ -3,14 +3,14 @@ close all;
 clc;
 options = odeset('RelTol', 1e-4, 'NonNegative', [1 2 3]);
 figure
-[t, x] = ode45('siv', [0 171], [8000 1000 1000], options);
+[t, x] = ode45('siv', [0 180], [8000 1000 1000], options);
 plot(t, x(:,1), 'b.', t, x(:,2), 'r.', t, x(:,3), 'g.')
 legend('S_1', 'I_1', 'V_1')
 hold on
-[t, x] = ode45('siv', [0 171], [1000 2000 7000], options);
+[t, x] = ode45('siv', [0 180], [1000 2000 7000], options);
 plot(t, x(:,1), 'b:', t, x(:,2), 'r:', t, x(:,3), 'g:')
 hold on
-[t, x] = ode45('siv', [0 171], [5500 500 4000], options);
+[t, x] = ode45('siv', [0 180], [5500 500 4000], options);
 plot(t, x(:,1), 'b-', t, x(:,2), 'r-', t, x(:,3), 'g-')
 xlabel('Months')
 ylabel('Individuals Infected')
